82d0d6613d77e0a4fc8d06d925e8786e74380613
SonCN <sonchaungoc@gmail.com>
2020-05-03 00:32:00 +0700

    finished 001

--- omg_vn/001.py
+++ omg_vn/001.py
@@ -3,11 +3,12 @@ from sqlalchemy import create_engine
 from credential import usrnm, pwd, host, db
 import pandas as pd
 
-engine = sal.create_engine('postgresql://'+ usrnm('th') +':'+pwd('th')+'@'+host()+':5432/'+db('th'))
+engine = sal.create_engine('postgresql://'+ usrnm('vn') +':'+pwd('vn')+'@'+host()+':5432/'+db('vn'))
 conn = engine.connect()
 
 command = """select "userID","chargeDate", "grossRev" from dbg_add"""
 df = pd.DataFrame(engine.execute(command), columns = ['userID', 'chargeDate', 'grossRev'])
-print(df.head(5))
-
+result = df.groupby(["userID","chargeDate"]).sum()
+result_sort = result.sort_values(by = ['chargeDate', 'userID'], ascending = [True, True])
+print(result_sort)
 conn.close()
