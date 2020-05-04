ced36712237de0f5fd37aa50b9b0320ed5fc5373
SonCN <sonchaungoc@gmail.com>
2020-05-04 21:16:48 +0700

    restructure

--- omg_vn/004.py
+++ omg_vn/004.py
@@ -4,14 +4,12 @@ import pandas as pd
 from credential import usrnm, pwd, host, db
 from date_mod import date_part, date_trunc
 
-
 engine = sal.create_engine('postgresql://' +usrnm('vn')+':'+pwd('vn')+'@'+host()+':5432/'+db('vn'))
 conn = engine.connect()
 command = """select count("userID"), "activeDate" from user_active_events group by 2 """
 
 dau = pd.DataFrame(engine.execute(command), columns = ['DAU', 'activeDate'])
 dau["activeDate"]= pd.DatetimeIndex(dau["activeDate"])
-
 
 print("Daily active users (per day):")
 print(dau)
