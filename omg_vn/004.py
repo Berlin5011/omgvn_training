9cb19ed70ef925d1e274ee40730ff012724148b5
SonCN <sonchaungoc@gmail.com>
2020-05-04 15:27:08 +0700

    finished DAU and avgDAU per month

--- omg_vn/004.py
+++ omg_vn/004.py
@@ -7,17 +7,21 @@ from date_mod import date_part, date_trunc
 
 engine = sal.create_engine('postgresql://' +usrnm('vn')+':'+pwd('vn')+'@'+host()+':5432/'+db('vn'))
 conn = engine.connect()
-
 command = """select count("userID"), "activeDate" from user_active_events group by 2 """
+
 dau = pd.DataFrame(engine.execute(command), columns = ['DAU', 'activeDate'])
 dau["activeDate"]= pd.DatetimeIndex(dau["activeDate"])
+
+
+print("Daily active users (per day):")
+print(dau)
+
 avg_dau = dau
-#avg_dau[["activeDate"]] = avg_dau[["activeDate"]].date()
-#print("DAU")
-#print(dau)
-avg_dau["start_month"] = avg_dau["activeDate"].apply(date_trunc, args = ('month',))
-avg_dau = avg_dau.groupby(["start_month"]).sum()
-#avg_dau["daynum"] = avg_dau["start_month"].apply(date_part, args = ('eom',))
-print("avg_DAU")
-print(avg_dau.shape)
+avg_dau["Month"] = avg_dau["activeDate"].apply(date_trunc, args = ('month',))
+avg_dau = avg_dau.groupby(["Month"]).sum().reset_index()
+avg_dau["DAU"] = avg_dau["DAU"] / avg_dau["Month"].apply(date_part, args = ('eom',))
+
+print("Average daily active users (per month):")
+print(avg_dau)
+
 conn.close()
