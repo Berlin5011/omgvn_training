a7cf7cd20d8113ed970c157dc7c7d753c69490f4
SonCN <sonchaungoc@gmail.com>
2020-05-04 14:45:32 +0700

    add date_mod to perform date_trunc, date_part

---
+++ omg_vn/date_mod.py
@@ -0,0 +1,18 @@
+import datetime
+def date_part(val, part):
+    if part == 'day':
+        return val.day
+    elif  part =='month':
+        return val.month
+    elif part == 'year':
+        return val.year
+    elif part == 'dow':
+        return val.weekday() #0 is Monday and 6 is Sunday... weird.
+    elif part == 'eom':
+        return (date_trunc(val + datetime.timedelta(days = 32),'month') - datetime.timedelta(days = 1)).day
+
+def date_trunc(val, part):
+    if part == 'week':
+        return val - datetime.timedelta(days = date_part(val,'dow'))
+    elif part == 'month':
+        return val - datetime.timedelta(days = date_part(val, 'day')-1)
