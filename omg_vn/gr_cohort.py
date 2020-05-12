374c68b06a7ca45ab63f4057143973a7535a8d09
SonCN <sonchaungoc@gmail.com>
2020-05-12 10:05:23 +0700

    new approach to the gr cohort problems

--- omg_vn/gr_cohort.py
+++ omg_vn/gr_cohort.py
@@ -6,22 +6,29 @@ from datetime import timedelta
 import time
 #------------------------------------------------------------------
 start = time.time()
-engine = create_engine('postgresql://'+usrnm('vn')+':'+pwd('vn')+'@'+host()+':5432/'+db('vn'))
+engine = create_engine('postgresql://'+usrnm('th')+':'+pwd('th')+'@'+host()+':5432/'+db('th'))
 conn = engine.connect()
 command_u = """select 	dbg_add."userID",
-		                date_trunc('day', user_profile."firstLoginDate"),
+		        date_trunc('day', user_profile."firstLoginDate"),
                         dbg_add."chargeDate",
-                        sum(dbg_add."grossRev") as "gr"
+                        sum(dbg_add."grossRev")*752 as "gr"
                 from dbg_add
                 inner join user_profile on dbg_add."userID" = user_profile."userID"
                 group by 1,2,3 """
 dfu = pd.DataFrame(engine.execute(command_u), columns = ['userID', '1stLogin','chargeDate','gr']).sort_values(by = ['1stLogin', 'chargeDate'], ascending = [True, True])
+command_nru = """select 	count(*) as "NRU",
+                            date_trunc('day', "firstLoginDate") as "1stLogin"
+                from user_profile
+                group by 2"""
+nru = pd.DataFrame(engine.execute(command_nru), columns = ['NRU', '1stLogin']).sort_values(by = ['1stLogin'], ascending = [True])
 conn.close()
 end = time.time()
 print("Data query cost = "+str(end-start)+'s')
+
 dfu['sub'] = dfu['chargeDate'] - dfu['1stLogin']
-nru = dfu.groupby(by = ['1stLogin'])['userID'].count()
-ans = dfu.groupby(by = ['1stLogin', 'sub'])['gr'].sum().reset_index()
-print(ans[ans['sub']<= timedelta(days = 30)].groupby(by = ['1stLogin']).sum())
+gr = dfu.groupby(by = ['1stLogin', 'sub'])['gr'].sum().reset_index()
+ans = gr.join(nru.set_index('1stLogin'), on = '1stLogin')
+ans['LTV'] = ans['gr']/ans['NRU']
 end2 = time.time()
+print(ans)
 print(end2 - end)
